9990af26ab0e1637e771e981ccfd7fe3c9bcf45b
Andr3w-Pham <andrew.pham2492@gmail.com>
2018-07-11 16:59:57 +1000

    add carrierwave

--- app/models/dj.rb
+++ app/models/dj.rb
@@ -1,3 +1,4 @@
 class Dj < ApplicationRecord
   belongs_to :user
+  mount_uploader :image, ImageUploader
 end
--- app/models/host.rb
+++ app/models/host.rb
@@ -1,3 +1,4 @@
 class Host < ApplicationRecord
   belongs_to :user
+  mount_uploader :image, ImageUploader
 end
