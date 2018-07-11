81c5a9ee2c0f6aa5e5b394e07cfc68e8b919ce20
Andr3w-Pham <andrew.pham2492@gmail.com>
2018-07-11 18:01:06 +1000

    add sign in for google

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :bookings
   resources :djs
   resources :hosts
-  devise_for :users
   root 'home#index'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
