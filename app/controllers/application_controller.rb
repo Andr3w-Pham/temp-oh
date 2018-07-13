d06294ba994a942391ec7cb5f269bfe38c1de434
Andr3w-Pham <andrew.pham2492@gmail.com>
2018-07-13 22:39:06 +1000

    fix sign in with google

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
    # before_action :authenticate_user!, except: [:index, :destroy]
 
    def after_sign_in_path_for(resource)
-     request.env['omniauth.origin'] || stored_location_for(resource) || home_profile_path
+    if current_user.host
+       request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    elsif current_user.dj
+      request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    else
+      request.env['omniauth.origin'] || stored_location_for(resource) || home_profile_path
+    end
    end
 end
