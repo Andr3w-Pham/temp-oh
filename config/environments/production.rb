fba3f58978db054f4fbddfb9690e7f3621db0dcb
Andr3w-Pham <andrew.pham2492@gmail.com>
2018-07-24 20:46:48 +1000

    heroku test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'temp-oh.herokuapp.com' }
   # Gmail configuration
 
   config.action_mailer.raise_delivery_errors = true
@@ -105,9 +104,9 @@ Rails.application.configure do
   #   authentication:       'plain',
   #   enable_starttls_auto: true
   # }
- config.action_mailer.default_url_options = { host: 'temp-oh.herokuapp.com' }
- # Gmail configuration
-  config.action_mailer.raise_delivery_errors = true
+ # config.action_mailer.default_url_options = { host: 'temp-oh.herokuapp.com' }
+ # Mailgun configuration
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.mailgun.org',
